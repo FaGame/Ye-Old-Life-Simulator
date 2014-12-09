e8814beb7c940d0238689d88842b9d6fb61d7428
bryanpope <pet4032@gmail.com>
2014-12-09 15:01:43 -0500

    added Building script

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Building.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour 
+{
+    public JobData[] m_JobData;
+
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    void Work(PlayerData pData, JobData jData)
+    {
+
+    }
+
+    void BuyItem(PlayerData, ItemData iData)
+    {
+
+    }
+
+    void Interact(PlayerData, InteractionData iData)
+    {
+
+    }
+
+    void ApplyForJob(PlayerData, JobData jData)
+    {
+
+    }
+}
