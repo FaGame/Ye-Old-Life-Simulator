23b1d6411ae07e83ceb7eda36643180bafb9033f
AHBrandon <brandoncoulthard@hotmail.com>
2014-12-09 14:20:22 -0500

    Starting Scripts for Player

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour 
+{
+    float acceleration_;
+    float currTime_;
+    float maxTime_;
+
+    bool isInfected_ = false;
+
+    public ArrayList[] m_Skill;
+
+    public string m_JobName;
+
+    Canvas m_PlayerCanvas;
+
+
+	void Start () 
+    {
+	
+	}
+	
+	void Update () 
+    {
+	
+	}
+
+    void UpdateTime()
+    {
+
+    }
+
+    void UpdateMovement()
+    {
+
+    }
+}
