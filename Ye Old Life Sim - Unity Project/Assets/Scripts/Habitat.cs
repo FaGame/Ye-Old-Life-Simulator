0b69cc5f28f517750b9d234cac19fa0448f2a5f3
AHBrandon <brandoncoulthard@hotmail.com>
2014-12-09 16:44:07 -0500

    Player Data and Habitat

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Habitat.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Habitat : MonoBehaviour 
+{
+    //Habitat
+    public int m_Rating;   
+    public int m_Penalty;
+    public float m_Rent;
+
+}
