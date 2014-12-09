3df670bbb05b5d147020e63b403038863ece68f9
mathewmanton <mathewmanton@hotmail.com>
2014-12-09 15:53:35 -0500

    Objective Script

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Objectives.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Objectives : MonoBehaviour {
+
+    //Habitat
+    public int m_Rating
+    {
+        get
+        {
+            return m_Rating;
+        }
+        set
+        {
+            m_Rating = value;
+        }
+    }
+
+    public int m_Penalty
+    {
+        get
+        {
+            return m_Rating;
+        }
+        set
+        {
+            m_Penalty = value;
+        }
+    }
+
+    public float m_Rent
+    {
+        get
+        {
+            return m_Rent;
+        }
+        set
+        {
+            m_Rent = value;
+        }
+    }  
+
+    //Hunger
+    public float m_HungerMeter
+    {
+        get
+        {
+            return m_HungerMeter;
+        }
+        set
+        {
+            m_HungerMeter = value;
+        }
+    }
+
+    public float m_MaxHunger
+    {
+        get
+        {
+            return m_MaxHunger;
+        }
+    }
+
+    //Rep
+    public int m_Rep
+    {
+        get
+        {
+            return m_Rep;
+        }
+        set
+        {
+            m_Rep = value;
+        }
+    }
+
+    //Currency
+    public int m_Shillings
+    {
+        get
+        {
+            return m_Shillings;
+        }
+        set
+        {
+            m_Shillings = value;
+        }
+    }  
+    
+    //Happiness
+    public float m_Happiness
+    {
+        get
+        {
+            return m_Happiness;
+        }
+        set
+        {
+            m_Happiness = value;
+        }
+    }
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+
+
+}
