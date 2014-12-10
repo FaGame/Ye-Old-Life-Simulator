3c36077af8bfb047d615ca7b2a9afa32b18a58a5
AHBrandon <brandoncoulthard@hotmail.com>
2014-12-10 16:22:56 -0500

    Finished Food Script, Probably

--- Ye Old Life Sim - Unity Project/Assets/Scripts/Items/Food.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Items/Food.cs
@@ -4,16 +4,16 @@ using System.Collections;
 public class Food : Item 
 {
     public bool m_StartTimer = false;       //used to call the UpdateTimer function
-    public bool m_SlowDownPlayer = false;   //set to true if you want the player speed to decrease
 
     public NavMeshAgent m_NavMeshAgent;     //player NavMeshAgent
 
+    public float m_SwitchTimerOff = 10.0f;  //time you want the UpdateTimer to stop at
+    public float m_Modifier = 0.0f;         //scalar to change player speed
     public float m_Time = 0.0f;             //used to keep track of time
     public float m_HungerAmount = 0.0f;     //how much you want to subtract from player hunger
 
     public Vector3 m_DefaultPlayerSpeedAmount = new Vector3(0.0f, 0.0f, 0.0f);   //default player speed
-    public Vector3 m_DecreaseSpeedAmount = new Vector3(0.0f, 0.0f, 0.0f);       //how slow you want the player to move
-    public Vector3 m_IncreaseSpeedAmount = new Vector3(0.0f, 0.0f, 0.0f);       //how fast you want the player to move
+   
 
     void Update()
     {
@@ -26,18 +26,10 @@ public class Food : Item
     //when the function is called subtract a value from the hunger meter and increase or decrease player speed
     public override void UseItem(PlayerData player)
     {
-        //Get the player's NavmeshAgent
+        //Get the player's NavmeshAgent and set the speed based on the modifier
         m_NavMeshAgent = player.GetComponent<NavMeshAgent>();
+        m_NavMeshAgent.speed += m_NavMeshAgent.speed * m_Modifier;
 
-        //if you want the player to slow down or speed up change the bool 'm_SlowDownPlayer'
-        if(m_SlowDownPlayer)
-        {
-            m_NavMeshAgent.velocity = m_DecreaseSpeedAmount;
-        }
-        else
-        {
-            m_NavMeshAgent.velocity = m_IncreaseSpeedAmount;
-        }
         m_StartTimer = true;
         player.m_HungerMeter -= m_HungerAmount;
     }
@@ -45,13 +37,12 @@ public class Food : Item
     public override void UpdateTimer()
     {
         //this function increases the m_Time variable for a set amount of time then flips the timer off and resets player speed to its default speed.
-        float switchTime = 10.0f;
         m_Time += Time.deltaTime;
 
-        if(m_Time >= switchTime)
+        if (m_Time >= m_SwitchTimerOff)
         {
-            //reset the player's speed to its default and reset timer
-            m_NavMeshAgent.velocity = m_DefaultPlayerSpeedAmount;
+            //reset the player's speed to its default speed and reset timer
+            m_NavMeshAgent.speed = ValueConstants.PLAYER_DEFAULT_SPEED;
             m_Time = 0.0f;
             m_StartTimer = false;
         }
