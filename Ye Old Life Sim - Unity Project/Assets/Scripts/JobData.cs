d419299d2d73c4104f5c13363e24b7c85cd6adb0
Spyb0y <IamAspyboy@gmail.com>
2014-12-10 15:26:51 -0500

    Jobs 1.1.7

--- Ye Old Life Sim - Unity Project/Assets/Scripts/JobData.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/JobData.cs
@@ -3,12 +3,9 @@ using System.Collections;
 
 public class JobData : MonoBehaviour 
 {
-    //public Requirement[] m_Requirement;
-    //public SkillGain[] m_SkillGain;
     public SkillAndAmount[] m_SkillRequirement;
     public SkillAndAmount[] m_SkillGain;
 
-    public float m_ReputationRequirement;
     public float m_Wage;
     public float m_MinWage;
     public float m_MaxWage;
@@ -16,16 +13,14 @@ public class JobData : MonoBehaviour
 
 	void Start () 
     {
-	
-	}
-	
-	void Update () 
-    {
-        
+        m_MinWage = 0;
+        m_MaxWage = 0;
 	}
 
+    //public function setting the wage and returning it
     public float SetWage()
     {
+        //if the wage has a min and max value inputed, it selecets a random number between them
         if(m_MinWage != m_MaxWage)
         {
             m_Wage = Random.Range(m_MinWage, m_MaxWage);
