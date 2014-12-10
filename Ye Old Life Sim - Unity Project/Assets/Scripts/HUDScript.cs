8bea9465fdc5211757b08236cb70d92a08bde4a7
Andrew Best <andrew.c.best@gmail.com>
2014-12-10 15:17:00 -0500

    Added skills to the Stats Menu

--- Ye Old Life Sim - Unity Project/Assets/Scripts/HUDScript.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/HUDScript.cs
@@ -9,6 +9,7 @@ public class HUDScript : MonoBehaviour
     public Canvas m_PlayerHUD; //Player's HUD canvas
     public GameObject m_Stats; //The stat's panel
     public GameObject m_Goals; //The goals panel
+    public GameObject m_Skills; //The skills panel
     public PlayerData m_PlayerData; //The player's data
 
     private bool statsActive_ = false; //This bool determines whether or not the the stats window is open
@@ -20,7 +21,9 @@ public class HUDScript : MonoBehaviour
     private Slider repObjSlider_; //Slider for the reputation objective
     private Slider currObjSlider_; //Slider for the currency/shillings objective
     private Slider happyObjSlider_; //Slider for the happiness objective
-    private Text[] textArray_; //Array of text for the stats screen
+    private Text[] statsText_; //Array of text for the stats screen
+    private Text[] skillsText_; //Array of text for the skills screen
+    private string skillString_; //String that will contain and display the skill names
     private List<float> playerStats_ = new List<float>(); //List of the player stats
 
     //All values in the following temp code should be set from the player rather than in here
@@ -48,11 +51,15 @@ public float m_HappinessObjective;
         playerStats_.Add(m_PlayerData.m_Shillings);
         playerStats_.Add(m_PlayerData.m_Happiness);
         
-        //Initialize the text array
-        textArray_ = m_Stats.GetComponentsInChildren<Text>();
-        //Turn off the stats and goal panels after initializing all stats
+        //Initialize the text arrays
+        statsText_ = m_Stats.GetComponentsInChildren<Text>();
+        skillsText_ = m_Skills.GetComponentsInChildren<Text>();
+
+        //Turn off the stats, goal and skills panels after initializing all stats
         m_Stats.SetActive(false);
         m_Goals.SetActive(false);
+        m_Skills.SetActive(false);
+
         //Set the current turn's timer
         timer_ = m_PlayerData.m_MaxTime;
 	}
@@ -92,14 +99,8 @@ public float m_HappinessObjective;
 	// Update is called once per frame
 	void Update ()
     {
-        timer_--;
-        timeSlider_.value = timer_;
+        timeSlider_.value = m_PlayerData.m_CurrTime;
         hungerSlider_.value = m_PlayerData.m_HungerMeter;
-        if (timer_ <= 0.0f)
-        {
-            m_PlayerData.m_HungerMeter += 10.0f;
-            timer_ = m_PlayerData.m_MaxTime;
-        }
 
         if (m_PlayerData.m_HungerMeter >= m_PlayerData.m_MaxHunger)
         {
@@ -109,17 +110,15 @@ public float m_HappinessObjective;
 
     void FixedUpdate()
     {
-        /*if(Input.GetKeyDown(KeyCode.Space))
-        {
-            m_CurrHunger -= 10.0f;
-        }*/
-
+        //If the player presses E (temporarily) open up the stats menu
         if(Input.GetKeyDown(KeyCode.E))
         {
             statsActive_ = !statsActive_;
             m_Stats.SetActive(statsActive_);
             m_Goals.SetActive(statsActive_);
+            m_Skills.SetActive(statsActive_);
             PopulateStats();
+            PopulateSkills();
             UpdateSliders();
         }
     }
@@ -136,9 +135,27 @@ public float m_HappinessObjective;
         playerStats_.Add(m_PlayerData.m_Happiness);
 
         //Loop through the pair of arrays to get the appropriate values next to the proper text
-        for (int i = 0; i < textArray_.Length - 1; ++i)
+        for (int i = 0; i < statsText_.Length - 1; ++i)
         {
-            textArray_[i].text = textArray_[i].name + ": " + playerStats_[i].ToString();
+            statsText_[i].text = statsText_[i].name + ": " + playerStats_[i].ToString();
+        }
+    }
+
+    //This function gets the skill names and values when the player opens the stats menu
+    void PopulateSkills()
+    {
+        if(m_PlayerData.m_Skills.Length <= 0)
+        {
+            return;
+        }
+        else
+        {
+            for(int i = 0; i < m_PlayerData.m_Skills.Length; ++i)
+            {
+                skillString_ += m_PlayerData.m_Skills[i].m_Skill.ToString() + ": " + m_PlayerData.m_Skills[i].m_Amount.ToString() + "\n";
+            }
+
+            skillsText_[1].text = skillString_;
         }
     }
 
