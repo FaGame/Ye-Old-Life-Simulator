b9e6ddc4fabc56e7f9995878bb5ef1d08a4d8ac8
Andrew Best <andrew.c.best@gmail.com>
2014-12-10 22:06:41 -0500

    Building Description/Job List now show

--- Ye Old Life Sim - Unity Project/Assets/Scripts/BuildingUI.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/BuildingUI.cs
@@ -5,23 +5,52 @@ using System.Collections;
 public class BuildingUI : MonoBehaviour 
 {
     public GameObject m_BuildingGUI;
+    public GameObject m_ApplyMenu;
     public Button m_WorkButton;
     public Button m_ApplyButton;
 
     private bool buildingsActive_ = false;
     private bool jobApplied_ = false;
+    private GameObject selectedBuilding_;
+    private Text[] buildingMenuText_;
+    private Text[] applyMenuText_;
+    private Text descriptionText_;
 
 	// Use this for initialization
 	void Start () 
     {
+        buildingMenuText_ = m_BuildingGUI.GetComponentsInChildren<Text>();
+        
+        descriptionText_ = buildingMenuText_[0];
         m_BuildingGUI.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        m_BuildingGUI.SetActive(buildingsActive_);
+        if (Input.GetMouseButtonDown(0) && !buildingsActive_)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayHit;
+            if (Physics.Raycast(ray, out rayHit))
+            {
+                if (rayHit.collider.tag == "Building")
+                {
+                    LoadBuildingData(rayHit.collider.name);
+                }
+            }
+        }
 	}
+
+    void LoadBuildingData(string name)
+    {
+        buildingsActive_ = true;
+        selectedBuilding_ = GameObject.Find(name);
+        descriptionText_.text = selectedBuilding_.GetComponent<Building>().GetDescription();
+
+        Debug.Log("Loading " + selectedBuilding_.name + "'s data");
+    }
 
     public void BuildingGUI()
     {
@@ -29,13 +58,23 @@ public class BuildingUI : MonoBehaviour
         m_BuildingGUI.SetActive(buildingsActive_);
     }
 
-    public void ApplyForJob()
+    //This function is used on button press in the building UI
+    //If pressed, it will populate and show the Apply for Job menu, in which you can choose a job to apply for
+    public void ApplyMenu()
     {
         jobApplied_ = true;
         m_WorkButton.interactable = true;
-        if (jobApplied_)
+        m_ApplyMenu.SetActive(true);
+        applyMenuText_ = m_ApplyMenu.GetComponentsInChildren<Text>();
+
+        for (int i = 0; i < selectedBuilding_.GetComponent<Building>().m_JobData.Length; ++i)
         {
-            m_ApplyButton.interactable = false;
+            applyMenuText_[i].text = selectedBuilding_.GetComponent<Building>().m_JobData[i].name;
         }
+    }
+
+    public void ApplyForJob()
+    {
+        //selectedBuilding_.GetComponent<Building>().ApplyForJob(selectedBuilding_.GetComponent<Building>().m_JobData);
     }
 }
