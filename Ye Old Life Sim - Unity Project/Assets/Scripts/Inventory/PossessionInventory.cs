da7c19c4c536ed7ac8de5ea5e9ca6f74229fc672
AHBrandon <brandoncoulthard@hotmail.com>
2014-12-11 16:06:14 -0500

    Possession Inventory Made

--- Ye Old Life Sim - Unity Project/Assets/Scripts/Inventory/PossessionInventory.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Inventory/PossessionInventory.cs
@@ -5,5 +5,59 @@ using System.Collections.Specialized;
 
 public class PossessionInventory : MonoBehaviour 
 {
+    [System.Serializable]
+    public struct ItemInventoryEntry
+    {
+        public Item item;
+        public int count;
+    };
 
+    public string[] m_Names;
+    public ItemInventoryEntry[] m_InspectorInventory;
+    public Dictionary<string, PossessionInventory.ItemInventoryEntry> m_PossessionItemInventory;
+
+    void Start()
+    {
+        m_PossessionItemInventory = new Dictionary<string, ItemInventoryEntry>();
+
+        //loops through the array of names and InspectorInventory, then adds them to the m_UseableItemInventory list
+        for (int i = 0; i < m_InspectorInventory.Length; ++i)
+        {
+            for (int j = 0; j < m_Names.Length; ++j)
+            {
+                AddToInventory(m_Names[j], m_InspectorInventory[i]);
+            }
+        }
+    }
+
+    public void AddToInventory(string name, ItemInventoryEntry item)
+    {
+        //adds an item to the list based on a name given and takes in a list of type ItemInventoryEntry
+        if (!m_PossessionItemInventory.ContainsKey(name))
+        {
+            m_PossessionItemInventory.Add(name, item);
+        }
+        else
+        {
+            ItemInventoryEntry entry = item;
+            item.count += m_PossessionItemInventory[name].count;
+            m_PossessionItemInventory[name] = item;
+        }
+    }
+
+    public void RemoveFromInventory(string name)
+    {
+        //removes an item from the ItemInventoryEntry based on the name given
+        if (m_PossessionItemInventory.ContainsKey(name))
+        {
+            ItemInventoryEntry item = m_PossessionItemInventory[name];
+            item.count--;
+            m_PossessionItemInventory[name] = item;
+
+            if (m_PossessionItemInventory.Count <= 0)
+            {
+                m_PossessionItemInventory.Remove(name);
+            }
+        }
+    }
 }
