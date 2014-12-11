291ceb89d1ddbe92d727020f4d8683e983d23422
Cyginus <cyginus@gmail.com>
2014-12-11 14:51:33 -0500

    Habitat update

--- Ye Old Life Sim - Unity Project/Assets/Scripts/PlayerData.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/PlayerData.cs
@@ -12,32 +12,27 @@ public class PlayerData : MonoBehaviour
 
     public Canvas m_PlayerCanvas;
 
-    public float m_DefaultSpeed = 10.0f;
     public float m_CurrTime = 0.0f;
     public float m_MaxTime = ValueConstants.PLAYER_MAX_TIME;
     public float m_HungerMeter = 0.0f;
     public float m_MaxHunger = ValueConstants.PLAYER_MAX_HUNGER;
     public float m_FoodPenalty = 0.0f;
     public float m_Happiness = 0.0f;
-    public float m_Speed;
 
     public int m_Reputation = 0;
     public int m_Shillings = 0;
-    public float m_EarningScalar = ValueConstants.PLAYER_DEFAULT_MONEY_SCALAR;    //scalar that is used to determine how much the player will earn that turn for work
+    public int m_EarningScalar = ValueConstants.PLAYER_DEFAULT_MONEY_SCALAR;    //scalar that is used to determine how much the player will earn that turn for work
 
     public bool m_IsInfected = false;       //variable used for when the player catches a disease
 
-    public List<ItemEffect> m_StatusEffects = new List<ItemEffect>();
-
 	void Start () 
     {
-        m_Speed = m_DefaultSpeed;
         StartTurn();
 	}
 	
 	void Update () 
     {
-        UpdateStatusEffects();
+	
 	}
 
     public void CalculateFoodPenalty()
@@ -56,45 +51,6 @@ public class PlayerData : MonoBehaviour
     {
         m_EarningScalar = ValueConstants.PLAYER_DEFAULT_MONEY_SCALAR;
         //calculate the curr time 
-        m_CurrTime = m_MaxTime - m_Home.m_Penalty - m_FoodPenalty;
+        m_CurrTime = m_MaxTime - m_Home.CalculateHomePenalty() - m_FoodPenalty;
     }  
-
-    public void AddEffect(ItemEffect itemEffect)
-    {
-        m_StatusEffects.Add(itemEffect);
-
-        switch(itemEffect.m_Type)
-        {
-            case ItemEffect.EffectType.INCOME_MODIFIER:
-                m_EarningScalar = itemEffect.m_Value;
-                break;
-            case ItemEffect.EffectType.SPEED:
-                m_Speed *= itemEffect.m_Value;
-                break;
-        }
-    }
-
-    public void UpdateStatusEffects()
-    {
-        for(int i = 0; i < m_StatusEffects.Count; ++i)
-        {
-            m_StatusEffects[i].m_Timer -= Time.deltaTime;
-            if(m_StatusEffects[i].m_Timer <= 0.0f)
-            {
-                switch (m_StatusEffects[i].m_Type)
-                {
-                    case ItemEffect.EffectType.INCOME_MODIFIER:
-                        m_EarningScalar = ValueConstants.PLAYER_DEFAULT_MONEY_SCALAR;
-                        break;
-
-                    case ItemEffect.EffectType.SPEED:
-                        m_Speed /= m_StatusEffects[i].m_Value;
-                        break;
-                }
-                m_StatusEffects.RemoveAt(i);
-                i--;
-            }
-        }
-    }
-
 }
