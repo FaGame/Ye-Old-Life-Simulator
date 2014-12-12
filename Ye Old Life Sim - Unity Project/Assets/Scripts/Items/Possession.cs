dfeedb9340f885c1e5807e654685a40a9219f047
AHBrandon <brandoncoulthard@hotmail.com>
2014-12-12 14:24:33 -0500

    Starts to a Possession Script

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Items/Possession.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Possession : MonoBehaviour 
+{
+    public int m_InitialCost = 0;
+    public int m_ResellValue = 0;
+    public int m_ReputationBonus = 0;
+
+    public SkillAndAmount m_Skill;
+
+    public float m_SkillAmount = 0.0f;
+    
+    void Start()
+    {
+        m_Skill.m_Amount = m_SkillAmount;
+        
+    }
+}
