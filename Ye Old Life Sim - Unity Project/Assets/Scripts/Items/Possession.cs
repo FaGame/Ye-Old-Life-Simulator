579383510965a0914ba4db5bf2975ca7a4bc967f
AHBrandon <brandoncoulthard@hotmail.com>
2014-12-13 16:22:53 -0500

    Fixed my Possession script

--- Ye Old Life Sim - Unity Project/Assets/Scripts/Items/Possession.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Items/Possession.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Possession : MonoBehaviour 
 {
@@ -10,7 +11,6 @@ public class Possession : MonoBehaviour
     public int m_ReputationBonus = 0;           //how much reputation bonus the player gets 
     public int m_SkillValueNeeded = 0;          //hopw much skill the player neeeds to use the bonus for that item 
 
-    public SkillAndAmount m_Skill;              //sets what skill and amount that item is for 
     public Skill.Skills m_SkillNeeded;          //determines what skill is needed for the item to be used by the player
 
     public float m_HappinessAmount = 0.0f;      //how much of a happpiness increase is applied to the player 
@@ -18,8 +18,11 @@ public class Possession : MonoBehaviour
     public string m_AquirredFrom = "";          //Just used to say who the item was aquirred from
     public string m_UsedBy = "";                //Who the item is used by 
 
+    private List<SkillAndAmount> skills_;       //sets what skill and amount that item is for 
+
     void Start()
     {
+        skills_ = m_Player.m_Skills;    //assigns the skills variable to the player's skills
         ApplyValueChange();
     }
 
@@ -28,10 +31,13 @@ public class Possession : MonoBehaviour
         //increases the player's happiness by the happiness variable
         m_Player.m_Happiness += m_HappinessAmount;
 
-        //checks if the player's skill matches the skill needed and that the player's skill amount for that job matches the skill value needed then increases reputation
-        if(m_Skill.m_Skill == m_SkillNeeded && m_Skill.m_Amount >= m_SkillValueNeeded)
+        //checks if one of the player's skills matches the skill needed and that the player's skill amount for that job matches the skill value needed then increases reputation
+        for (int i = 0; i < skills_.Count; ++i)
         {
-            m_Player.m_Reputation += m_ReputationBonus;
-        }
+            if (skills_[i].m_Skill == m_SkillNeeded && skills_[i].m_Amount >= m_SkillValueNeeded)
+            {
+                m_Player.m_Reputation += m_ReputationBonus;
+            }
+        }     
     }
 }
