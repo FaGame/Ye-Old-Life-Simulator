f36dc1796e18fb03cfe10c969aeb27e2de9a0532
BrendanM <danmatrix@live.com>
2014-12-13 22:19:14 -0500

    Game Menu Scene + Script and Updated Game Manager
    Game manager has win checks and sets win values, no loss yet since it
    hasn't been outlined

--- Ye Old Life Sim - Unity Project/Assets/Scripts/GameManager.cs
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/GameManager.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GameManager : MonoBehaviour 
+public class GameManager : MonoBehaviour
 {
     public GameObject m_Player;
     public GameObject m_EmptyObjPlayer;
@@ -13,6 +13,16 @@ public class GameManager : MonoBehaviour
     private bool isPlayerTurn_;
     private bool isAiTurn_;
 
+    private float maxRep_;
+    private float maxCurrency_;
+    private float maxHappy_;
+    private float maxTurns_;
+    private int Turns_;
+    private int IncrementTurns_ = 1;
+    private bool turnsGame_ = false;
+    private bool objectivesGame_ = false;
+    private bool sandboxGame_ = false;
+
     void Start()
     {
         m_PlayerData = m_EmptyObjPlayer.GetComponent<PlayerData>();
@@ -21,6 +31,10 @@ public class GameManager : MonoBehaviour
         isPlayerTurn_ = true;
         isAiTurn_ = false;
 
+        GameMenu.m_isTurnGame = turnsGame_;
+        GameMenu.m_isSandboxGame = sandboxGame_;
+        GameMenu.m_isObjectivesGame = objectivesGame_;
+
         //starts player at current home
         //m_Player.transform.position = m_PlayerData.m_Home.transform.position;
 
@@ -28,18 +42,26 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
+        TurnManager();
+        SetObjGame();
+        SetTurnsGame();
+    }
+
+    void TurnManager()
+    {
         //Debug.Log(m_PlayerData.m_CurrTime);
         //Debug.Log(m_AIData.m_CurrTime);
         //start turn is the player, once their time runs out, disables player and activates AI and AI turn
-        if(isPlayerTurn_ == true)
+        if (isPlayerTurn_ == true)
         {
             m_Player.SetActive(true);
 
             if (m_PlayerData.m_CurrTime <= 0)
             {
-                
+                Turns_ += IncrementTurns_;
+
                 m_Player.SetActive(false);
-                
+
                 isPlayerTurn_ = false;
 
                 isAiTurn_ = true;
@@ -65,5 +87,45 @@ public class GameManager : MonoBehaviour
                 m_AIData.StartTurn();
             }
         }
+    }
+
+    void CheckForObjWin()
+    {
+        if (m_PlayerData.m_Happiness >= maxHappy_ && m_PlayerData.m_Reputation >= (int)maxRep_ && m_PlayerData.m_Shillings >= (int)maxCurrency_)
+        {
+            //You Win Objectives gametype, do shit
+            Debug.Log("You Win!!");
+        }
+    }
+
+    void CheckForTurnWin()
+    {
+       if(Turns_ >= maxTurns_)
+       {
+           //You Win Turns gametype, do shit
+           Debug.Log("You Win!!");
+       }
+    }
+
+    void SetObjGame()
+    {
+        if(objectivesGame_ == true)
+        {
+            maxCurrency_ = GameMenu.m_selectedCurrency;
+            maxHappy_ = GameMenu.m_selectedHappiness;
+            maxRep_ = GameMenu.m_selectedRep;
+        }
+
+        CheckForObjWin();
+    }
+
+    void SetTurnsGame()
+    {
+        if(turnsGame_ == true)
+        {
+            maxTurns_ = GameMenu.m_selectedTurns;
+        }
+
+        CheckForTurnWin();
     }
 }
---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Canvas))]
+public class GameMenu : MonoBehaviour 
+{
+    public Canvas m_GameMenu;
+
+    public Slider m_repObjSlider;
+    public Slider m_currencyObjSlider;
+    public Slider m_happyObjSlider;
+    public Slider m_turnsSlider;
+
+    public Toggle m_playersOne;
+    public Toggle m_playersTwo;
+
+    public Toggle m_Objectives;
+    public Toggle m_Turns;
+    public Toggle m_Sandbox;
+
+    public GameObject m_objectivesSliders;
+    public GameObject m_turnSlider;
+
+    public Text m_currencyText;
+    public Text m_repText;
+    public Text m_happyText;
+    public Text m_turnsText;
+
+    public static float m_selectedRep;
+    public static float m_selectedCurrency;
+    public static float m_selectedHappiness;
+    public static float m_selectedTurns;
+    public static float m_Players;
+
+    private string currTextString_;
+    private string repTextString_;
+    private string happyTextString_;
+    private string turnsTextString_;
+
+    public static bool m_isObjectivesGame = false;
+    public static bool m_isTurnGame = false;
+    public static bool m_isSandboxGame = false;
+
+    void Update()
+    {
+        SetValues();
+        SetToggleValues();
+        ToggleSlidersOnOff();
+        SetPanelTexts();
+        SetGameType();
+    }
+
+    void SetValues()
+    {
+        m_selectedRep = m_repObjSlider.value;
+        m_selectedCurrency = m_currencyObjSlider.value;
+        m_selectedHappiness = m_happyObjSlider.value;
+        m_selectedTurns = m_turnsSlider.value;
+    }
+
+    void SetToggleValues()
+    {
+        if(m_playersOne.isOn)
+        {
+            m_Players = 1;
+        }
+        //--------------------
+        if(m_playersTwo.isOn)
+        {
+            m_Players = 2;
+        }
+    }
+
+    void ToggleSlidersOnOff()
+    {
+        if(m_Objectives.isOn)
+        {
+            m_objectivesSliders.SetActive(true);
+        }
+        else
+        {
+            m_objectivesSliders.SetActive(false);
+        }
+        //---------------------------------------
+        if(m_Turns.isOn)
+        {
+            m_turnSlider.SetActive(true);
+        }
+        else
+        {
+            m_turnSlider.SetActive(false);
+        }
+    }
+
+    void SetPanelTexts()
+    {
+        currTextString_ = "" + m_currencyObjSlider.value;
+        m_currencyText.text = currTextString_;
+
+        repTextString_ = "" + m_repObjSlider.value;
+        m_repText.text = repTextString_;
+
+        happyTextString_ = "" + m_happyObjSlider.value;
+        m_happyText.text = happyTextString_;
+
+        turnsTextString_ = "" + m_turnsSlider.value;
+        m_turnsText.text = turnsTextString_;
+
+
+    }
+
+    public void Play()
+    {
+        //Application.LoadLevel(1);
+    }
+
+    void SetGameType()
+    {
+        if(m_Sandbox.isOn == true)
+        {
+            m_isSandboxGame = true;
+        }
+
+        if(m_Turns.isOn == true)
+        {
+            m_isTurnGame = true;
+        }
+
+        if(m_Objectives.isOn == true)
+        {
+            m_isObjectivesGame = true;
+        }
+    }
+}
