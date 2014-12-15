20541048e26da11828ba950498276c9d42dd7ddc
JimCorrigan <cyginus@gmail.com>
2014-12-15 14:12:16 -0500

    Main Screen and Scripts added

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+     public void StartScene()
+    {
+        Debug.Log("Load Next Scene");
+    }
+
+    public void CloseGame()
+    {
+        Debug.Log("Close Game");
+    }
+
+}
