7ac059fe6a4598e5fd08bd6e4f71fede90123773
Spyb0y <IamAspyboy@gmail.com>
2014-12-16 14:10:27 -0500

    Inventory UI 1.1.2

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/Inventory/UseItemButton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UseItemButton : MonoBehaviour 
+{
+    public UseableItemInventory m_InventoryScript;
+
+    public void UseItem(PlayerData playerData)
+    {
+        string nameOfObject = gameObject.name;
+        if (nameOfObject == "InventoryItemButton")
+        {
+            //close inventory
+        }
+        else if(nameOfObject == null)
+        {
+            return;
+        }
+        else
+        {
+            foreach (KeyValuePair<string, UseableItemInventory.ItemInventoryEntry> currentItem in m_InventoryScript.m_UseableItemInventory)
+            {
+                if(currentItem.Key == nameOfObject)
+                {
+                    currentItem.Value.item.UseItem(playerData);
+                    m_InventoryScript.RemoveFromInventory(currentItem.Key);
+                }
+                else
+                {
+                    return;
+                }
+            }
+            return;
+        }
+    }
+}
