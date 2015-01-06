045766411026c28d0b25968f710827bdaa314e4d
bryanpope <pet4032@gmail.com>
2015-01-06 16:45:51 -0500

    Texture mouse pointer added :wine_glass:

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/UI/MouseCursor.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseCursor : MonoBehaviour 
+{
+    public Texture2D m_MouseCursorTexture;
+    public Vector2 m_HotSpot = Vector2.zero;
+    public CursorMode m_CursorMode = CursorMode.Auto;
+
+	// Use this for initialization
+	void Start () 
+    {
+        Cursor.SetCursor(m_MouseCursorTexture, m_HotSpot, m_CursorMode);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
