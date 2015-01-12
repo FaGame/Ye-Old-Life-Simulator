21e620045f0e0057f5120941aea45f4799bd3fab
mathewmanton <mathewmanton@hotmail.com>
2015-01-12 13:15:13 -0500

    New Game Menu

---
+++ Ye Old Life Sim - Unity Project/Assets/Scripts/GameMenuCam.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenuCam : MonoBehaviour {
+
+    public Camera m_mainCamera;
+
+   // private Vector3 camPosition_;
+	// Use this for initialization
+	void Start () 
+    {
+        //camPosition_ = m_mainCamera.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        m_mainCamera.transform.Rotate(0.0f, Time.deltaTime * 4.0f, 0.0f, Space.World);
+	}
+}
